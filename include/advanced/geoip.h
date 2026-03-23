aebdcbfbf18f47f5bda5d5c10a70940a855741c7
lunardev <gzuhravyi@gmail.com>
2026-03-23 22:03:23 +0100

    Remove unnecessary whitespace and format geoip.h

--- include/advanced/geoip.h
+++ include/advanced/geoip.h
@@ -2,18 +2,14 @@
 #define LEMONADE_GEOIP_H
 
 #include <string>
-#include <vector>
 #include <map>
 #include <cstdint>
 
 namespace lemonade {
 namespace GeoIP {
-
-extern std::map<uint32_t, std::pair<uint32_t, std::string>> geo_db;
-
-void load_geo();
-std::string lookup(const std::string& ip_str);
-
+    extern std::map<uint32_t, std::pair<uint32_t, std::string>> geo_db;
+    void load_geo();
+    std::string lookup(const std::string& ip_str);
 }
 }
 
